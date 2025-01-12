bda14ceb7d3362c172ed87253a3c5f8c61f2fca9
Bhosle Abhishek Wamanrao <abhishek.bhosle2001@gmail.com>
2025-01-12 23:21:36 +0530

    Database

---
+++ Nimap_Task_Data.sql
@@ -0,0 +1,15 @@
+create database Spring_Boot_Project;
+
+use Spring_Boot_Project;
+
+drop database Spring_Boot_Project;
+
+show tables;
+
+drop table category;
+
+select * from  category;
+select * from product;
+
+desc category;
+desc product;
